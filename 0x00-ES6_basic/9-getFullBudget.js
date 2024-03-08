396621faad7310db24fe1278a396a981729579d2
boak123 <boluwatifeakorede17@gmail.com>
2024-03-08 07:29:09 -0800

    getfullbudget

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -7,7 +7,7 @@ export default function getFullBudgetObject(income, gdp, capita) {
     getIncomeInDollars(income) {
       return `$${income}`;
     },
-    getIncomeInEuros: function (income) {
+    getIncomeInEuros(income) {
       return `${income} euros`;
     },
   };

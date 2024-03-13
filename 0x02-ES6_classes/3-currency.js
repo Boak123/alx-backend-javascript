6beda795d2f6b32911c4c53f42108840ca1f41aa
boak123 <boluwatifeakorede17@gmail.com>
2024-03-12 18:45:46 -0700

    updated

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -2,24 +2,25 @@ export default class Currency() {
   constructor(code, name) {
     this._code = code;
     this,_name = name;
-}
+  }
 
-get code() {
-  return this._code;
-}
+  get code() {
+    return this._code;
+  }
 
-set code(value) {
-  return this._code = value;}
-}
+  set code(value) {
+    return this._code = value;}
+  }
 
-get name() {
-  return this._name;
-}
+  get name() {
+    return this._name;
+  }
 
-set name(value) {
-  return this._name = value;
-}
+  set name(value) {
+    return this._name = value;
+  }
 
-displayFullCurrency() {
-  return '${this.name} (${this.code})';
+  displayFullCurrency() {
+    return '${this.name} (${this.code})';
+  }
 }
